d5810670e9da58c97d1c15a91639f87f8b8c2cb6
Alex <ialeonb353@gmail.com>
2026-09-12 21:25:13 -0500

    Calculadora basica de plazos

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 fecha_inicio = input("Ingresa la fecha de inicio (DD/MM/AAAA): ")
 dias = int(input("Ingresa el número de días del plazo: "))
 
 fecha = datetime.strptime(fecha_inicio, "%d/%m/%Y")
 
-print("\nDatos ingresados:")
+fecha_vencimiento = fecha + timedelta(days=dias)
+
+print("\nResultado:")
 print("Fecha de inicio:", fecha.strftime("%d/%m/%Y"))
 print("Días del plazo:", dias)
+print("Fecha de vencimiento:", fecha_vencimiento.strftime("%d/%m/%Y"))
