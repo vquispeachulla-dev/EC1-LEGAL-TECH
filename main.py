03e3d1b54d514a1ec6b39f48e25d9b7afc6e8160
Delia <deliaromeror06@gmail.com>
2026-09-15 22:41:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from datetime import datetime, timedelta
-
 FERIADOS_NACIONALES_2026 = {
     "01/01/2026", "02/04/2026", "03/04/2026", "01/05/2026", "07/06/2026", "29/06/2026", "23/07/2026", "28/07/2026", 
     "29/07/2026", "06/08/2026", "30/08/2026", "08/10/2026", "01/11/2026", "08/12/2026", "09/12/2026", "25/12/2026"
 }
 
-print(" CALCULADORA DE PLAZOS ADMINISTRATIVOS: RECURSOS ADMINISTRATIVOS")
+print("\nCALCULADORA DE PLAZOS ADMINISTRATIVOS: RECURSOS ADMINISTRATIVOS")
 tipo_recurso = input("\nEscoga entre: Apelación - Reconsideración: ")
 fecha_inicio = input("\nIngrese la fecha de notificación (DD/MM/AAAA): ")
 
 dias = 15 #plazo que da la ley para interponer los recursos
 
 print("\nMencione el tipo de cómputo que desea aplicar:")
-print(" 1. Días hábiles")
-print(" 2. Días calendario")
+print("      1. Días hábiles     2. Días calendario")
 
-tipo = input("Seleccione una opción (1 o 2): ")
+tipo = input("\nSeleccione una opción (1 o 2): ")
 
 try:
     fecha = datetime.strptime(fecha_inicio, "%d/%m/%Y")
 except ValueError:
-    print("Fecha inválida. Ingrese una fecha válida.")
+    print(" Fecha inválida. Ingrese una fecha válida.")
     exit()
 
 if tipo == "1":
     fecha_vencimiento = fecha
-    dias_contados = 0
+    dias_contados = 0 
 
     while dias_contados < dias:
         fecha_vencimiento += timedelta(days=1)
@@ -39,30 +37,28 @@ elif tipo == "2":
     fecha_vencimiento = fecha + timedelta(days=dias)
 
 else:
-    print("Opción no válida.")
+    print(" Opción no válida.")
     exit()
 
 print("\nResultado:")
-print("\n Fecha de inicio:", fecha.strftime("%d/%m/%Y"))
-print(" Días del plazo:", dias)
+print("\n     Fecha de notificación:", fecha.strftime("%d/%m/%Y"))
+print("     Días del plazo:", dias)
 
 if tipo == "1":
-    print(" Tipo de cómputo: Días hábiles")
+    print("     Tipo de cómputo: Días hábiles")
 else:
-    print(" Tipo de cómputo: Días calendario")
+    print("     Tipo de cómputo: Días calendario")
 
-print(" Fecha de vencimiento:", fecha_vencimiento.strftime("%d/%m/%Y"))
+print("     Fecha de vencimiento:", fecha_vencimiento.strftime("%d/%m/%Y"))
 
 from datetime import date
 
 hoy = date.today()
-dias_restantes = (fecha_vencimiento.date() - hoy).days #se le agrega el days para que lea el resultado como número
+dias_restantes = (fecha_vencimiento.date() - hoy).days
 
 if dias_restantes < 0 :
-   print("\n Tu plazo ya venció hace", abs(dias_restantes), "dias calendario") #Uso "abs" par que convierta el numero negativo a positivo
+   print("\nTu plazo ya venció hace", abs(dias_restantes), "dias calendario.")
 else :
-   print("\n Te quedan", dias_restantes, "días calendario")
+   print("\nTe quedan", dias_restantes, "días calendario.")
 
-print("\nBase legal: TUO de la Ley N.° 27444, artículos 133 y 134.")
-
-print("\nBase legal: TUO de la Ley N.° 27444, artículos 133 y 134.")
+print("\n*Base legal: TUO de la Ley N.° 27444, artículos 133, 134 y 139.2.")
